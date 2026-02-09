a7ee8b50db068914a13b989f3d500d6c525f01c4
Preetanshu Mishra <preetanshumishra@gmail.com>
2026-02-08 22:37:12 -0500

    Fix TruweatherMobile tab icons and JDK compatibility issues

--- TruweatherMobile/MauiProgram.cs
+++ TruweatherMobile/MauiProgram.cs
@@ -1,3 +1,4 @@
+using CommunityToolkit.Maui;
 using TruweatherCore.Http;
 using TruweatherMobile.Pages;
 using TruweatherMobile.Services;
@@ -12,6 +13,7 @@ public static class MauiProgram
         var builder = MauiApp.CreateBuilder();
         builder
             .UseMauiApp<App>()
+            .UseMauiCommunityToolkit()
             .ConfigureFonts(fonts =>
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
